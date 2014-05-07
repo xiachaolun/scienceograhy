f54434b3ff3952a406ef6fb51b7f1c719d926425
Chaolun Xia <sasnzy13@163.com>
2014-05-07 12:29:31 -0400

    creat

---
+++ crawlers/remote_launch/kill_all_workers.sh
@@ -0,0 +1,11 @@
+
+while read -r line
+do
+    array+=($line)
+    done < instagram_gp_host.txt
+    for ((i=0; i < ${#array[*]}; i++))
+    do
+        echo "${array[i]}"
+            # pkill -9 -f kills a job with partial name. So *make sure* that the name here is what you want to kill
+            ssh "${array[i]}" "pkill -9 -f '/grad/users/cx28/scienceograhy/crawlers/run_worker.py'"
+done
---
+++ crawlers/remote_launch/start_all_workers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while read -r line
+do
+    array+=($line)
+    done < api_host.txt
+    for ((i=0; i < ${#array[*]}; i++))
+    do
+        launch_cmd="source ~/.bash_profile; nohup python /grad/users/cx28/scienceograhy/crawlers/run_worker.py &"
+        ssh "${array[i]}" "$launch_cmd"
+    done
