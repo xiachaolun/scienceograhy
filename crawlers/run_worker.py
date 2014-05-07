89dbd0d3ddf43b726b3073f2b7137a1d938d1eeb
Chaolun Xia <sasnzy13@163.com>
2014-05-07 12:21:48 -0400

    creat

--- crawlers/run_worker.py
+++ crawlers/run_worker.py
@@ -2,13 +2,13 @@ import sys
 import os
 # add the utility library outside
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
+#sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
 
 from rq import Queue, Worker, Connection
 from redis import Redis
 
 from utility.config import redis_server
-from citation.crawler import *
+#from citation.crawler import *
 
 if __name__ == '__main__':
     redis_conn = Redis(redis_server)
