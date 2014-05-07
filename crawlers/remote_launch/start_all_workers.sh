31be5553be0bff175c249ab81820557826b01daf
Chaolun Xia <sasnzy13@163.com>
2014-05-07 12:38:32 -0400

    creat

--- crawlers/remote_launch/start_all_workers.sh
+++ crawlers/remote_launch/start_all_workers.sh
@@ -6,6 +6,10 @@ do
     done < api_host.txt
     for ((i=0; i < ${#array[*]}; i++))
     do
+        echo "${array[i]}"
+        current_path=$PWD
+        echo "current path = $current_path"
         launch_cmd="source ~/.bash_profile; nohup python /grad/users/cx28/scienceograhy/crawlers/run_worker.py > /.freespace/cx28/logs < /dev/null 2>&1 &"
-        ssh "${array[i]}" "$launch_cmd"
+        echo "cmd = $launch_cmd"
+        ssh "${array[i]}" "$launch_cmd" 
     done
