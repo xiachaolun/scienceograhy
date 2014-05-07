5bb9c8cf39943990e6605a9dff474455fbc5ab9b
Chaolun Xia <sasnzy13@163.com>
2014-05-07 12:34:13 -0400

    creat

--- crawlers/remote_launch/start_all_workers.sh
+++ crawlers/remote_launch/start_all_workers.sh
@@ -6,6 +6,6 @@ do
     done < api_host.txt
     for ((i=0; i < ${#array[*]}; i++))
     do
-        launch_cmd="source ~/.bash_profile; nohup python /grad/users/cx28/scienceograhy/crawlers/run_worker.py &"
+        launch_cmd="source ~/.bash_profile; nohup python /grad/users/cx28/scienceograhy/crawlers/run_worker.py < /dev/null 2>&1 &"
         ssh "${array[i]}" "$launch_cmd"
     done
