b0062d7d7cb2f35e3a859ad9c503ef8b9ca96469
Chaolun Xia <sasnzy13@163.com>
2014-05-09 01:22:20 -0400

    creat

--- utility/test.py
+++ utility/test.py
@@ -1,12 +1,30 @@
 import psutil
 
+def maintain():
+    cmdline = 'run_worker.py'
 
-cmdline = 'run_worker.py'
+    cnt = 0
+    for proc in psutil.process_iter():
+        if proc.username() == 'cx28':
+            for part in proc.cmdline():
+                if cmdline in part:
+                    cnt += 1
+    if cnt == 1:
+        print 'good'
+        return
 
-cnt = 0
-for proc in psutil.process_iter():
-    if proc.username() == 'cx28':
-        for part in proc.cmdline():
-            if cmdline in part:
-                cnt += 1
-print cnt
+    print 'need to kill'
+
+    for proc in psutil.process_iter():
+        if proc.username() == 'cx28':
+            for part in proc.cmdline():
+                if cmdline in part:
+                    if cnt > 1:
+                        cnt -= 1
+                        try:
+                            proc.kill()
+                        except Exception, e:
+                            print e
+
+if __name__ == '__main__':
+    maintain()
