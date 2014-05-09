888e3bcfb91459c730469818008267a4e19ae7df
Chaolun Xia <sasnzy13@163.com>
2014-05-09 01:08:48 -0400

    creat

--- utility/test.py
+++ utility/test.py
@@ -6,7 +6,7 @@ cmdline = '/grad/users/cx28/scienceograhy/'
 cnt = 0
 for proc in psutil.process_iter():
     if proc.username() == 'cx28':
-        for part in cmdline in proc.cmdline():
+        for part in proc.cmdline():
             if cmdline in part:
                 cnt += 1
 print cnt
