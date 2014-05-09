0307ff0e51d9273f82d7d133851819c663bb283b
Chaolun Xia <sasnzy13@163.com>
2014-05-09 01:40:10 -0400

    creat

--- crawlers/remote_launch/maintain_worker.py
+++ crawlers/remote_launch/maintain_worker.py
@@ -3,28 +3,23 @@ import psutil
 def maintain():
     cmdline = 'run_worker.py'
 
-    cnt = 0
+    procs = []
     for proc in psutil.process_iter():
         if proc.username() == 'cx28':
             for part in proc.cmdline():
                 if cmdline in part:
-                    cnt += 1
-    if cnt == 1:
+                    procs.append(proc)
+    if len(procs) <= 1:
         print 'good'
         return
 
     print 'need to kill'
 
-    for proc in psutil.process_iter():
-        if proc.username() == 'cx28':
-            for part in proc.cmdline():
-                if cmdline in part:
-                    if cnt > 1:
-                        cnt -= 1
-                        try:
-                            proc.kill()
-                        except Exception, e:
-                            print e
+    for i in xrange(len(procs) - 1):
+        try:
+            proc[i].kill()
+        except Exception, e:
+            print e
 
 if __name__ == '__main__':
     maintain()
