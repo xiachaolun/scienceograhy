ee346006ecb9caada5856b662554c9c094f801b0
Chaolun Xia <sasnzy13@163.com>
2014-05-09 01:06:50 -0400

    creat

--- utility/test.py
+++ utility/test.py
@@ -5,5 +5,5 @@ cmdline = '/grad/users/cx28/scienceograhy/crawlers/run_worker.py'
 cnt = 0
 for proc in psutil.process_iter():
     if proc.username() == 'cx28' and cmdline in proc.cmdline():
-            cnt += 1
+        cnt += 1
 print cnt
