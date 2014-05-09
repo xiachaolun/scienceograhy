1a2c404f4a74dce7614d7b6c93b6fc50ba931073
Chaolun Xia <sasnzy13@163.com>
2014-05-09 12:44:01 -0400

    creat

--- crawlers/crawler_venue.py
+++ crawlers/crawler_venue.py
@@ -14,6 +14,7 @@ import re
 def _crawlVenueInfoGivenId(venue_id):
     # venue id: Conference/197
     url = 'http://academic.research.microsoft.com/%s/' % venue_id
+    print url
     html = pq(url)
     venue_card = html('div').filter('.conference-card')
     # print author_card
