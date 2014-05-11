86a6f50aabd01e28f739e808c0ced142ab526be5
Chaolun Xia <sasnzy13@163.com>
2014-05-11 16:59:47 -0400

    creat

--- regressor/feature_extractor.py
+++ regressor/feature_extractor.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from utility.mongodb_interface import MongoDBInterface
 from utility.config import *
 
-from data_process.data_provider import getAllPaperAbstractInfo
+from data_process.data_provider import getAllPaperAbstractInfo, getAllVenueInfo
 
 from pprint import pprint
 
@@ -14,6 +14,7 @@ class FeatureExtractor(object):
     def __init__(self):
         self._retrieveAllMainPaper()
         self._retrieveAllCitingPaper()
+        self._retrieveAllVenue()
 
 
     def _retrieveAllMainPaper(self):
@@ -31,6 +32,9 @@ class FeatureExtractor(object):
 
     def _retrieveAllCitingPaper(self):
        self.all_paper_info = getAllPaperAbstractInfo({'meta.year':{'$lte':2009, '$gte':2000}})
+
+    def _retrieveAllVenue(self):
+        self.all_venue_info = getAllVenueInfo()
 
     def _computeCitingPaperTimeSeries(self):
         # get all citing papers of main papers between 2000 and 2009
@@ -58,16 +62,74 @@ class FeatureExtractor(object):
         # citation count ts
         paper_features = {}
         for paper in self.main_paper:
-            ts = []
+            ts_feature = {}
             sum = 0
             for year in xrange(2000, 2010):
                 year_citation_count = len(paper['citing_paper_time_series'][year])
-                ts.append(year_citation_count)
+                ts_feature[str(year)]= year_citation_count
                 sum += year_citation_count
-            ts.append(sum)
-            paper_features[paper['_id']] = ts
+            ts_feature['sum'] = sum
+            paper_features[paper['_id']] = ts_feature
 
-        #
+        # citing papers' venue. during first k years,
+        # how many citing papers are outside from computer science, and what is the percentage
+        k = 3
+        tire_1 = {'Journal': 5, 'Conference': 3} # top 70, since we have 3500 conference in CS
+        tire_2 = {'Journal': 10, 'Conference': 6}
+        for paper in self.main_paper:
+            sum = 0
+            outside_cs_count = 0
+            inside_cs_count = 0
+            tire_1_count = 0
+            tire_2_count = 0
+            accumulated_ranking_percentile = 0
+            journal_count = 0 # within cs domain
+            conference_count = 0
+            for year in xrange(2000, 2000 + k):
+                for citing_id in paper['citing_paper_time_series'][year]:
+                    citing_paper = self.all_paper_info.get['citing_id']
+                    venue_id = citing_paper['meta']['venue_id']
+                    type = citing_paper['meta']['venue_type']
+                    venue = self.all_venue_info[venue_id]
+                    if venue['domain'] == 3:
+                        # is from cs
+                        inside_cs_count += 1
+                        if type == 'Journal':
+                            journal_count += 1
+                        else:
+                            conference_count += 1
+                        accumulated_ranking_percentile += venue['field_ranking_percentile']
+                        if venue['field_ranking_percentile'] <= tire_1[type]:
+                            tire_1_count += 1
+                        if venue['field_ranking_percentile'] <= tire_2[type]:
+                            tire_2_count += 2
+                    else:
+                        outside_cs_count += 1
+                    sum += 1
+
+            # feature lsit
+            # smoothing and make it a real number
+            sum += 0.01
+            inside_cs_count += 0.01
+            outside_cs_percentage = outside_cs_count / sum
+            tire_1_count = tire_1_count # leave it as a feature
+            tire_1_percentage = tire_1_count/ inside_cs_count
+            tire_2_count = tire_2_count # leave it as a feature
+            tire_2_percentage = tire_2_count / inside_cs_count
+            average_ranking_percentile = accumulated_ranking_percentile / inside_cs_count
+            journal_count_percentage = journal_count / inside_cs_count
+
+            venue_feature = {}
+            venue_feature['outside_cs_percentage'] = outside_cs_percentage
+            venue_feature['tire_1_count'] = tire_1_count
+            venue_feature['tire_1_percentage'] = tire_1_percentage
+            venue_feature['tire_2_count'] = tire_2_count
+            venue_feature['tire_2_percentage'] = tire_2_percentage
+            venue_feature['average_ranking_percentile'] = average_ranking_percentile
+            venue_feature['journal_count_percentage'] = journal_count_percentage
+
+            paper_features[paper['_id']] = dict(paper_features[paper['_id']].items() + venue_feature.items())
+
         return paper_features
 
     def extractFeatures(self):
