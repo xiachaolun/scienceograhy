5e03311b15d254f192faa76cb9994350fd517082
Chaolun Xia <sasnzy13@163.com>
2014-05-21 19:40:46 -0400

    creat

--- test/test.py
+++ test/test.py
@@ -20,20 +20,26 @@ def preprocess():
     for id, t in fe.main_paper.items():
         ts = []
         normalized_ts = []
+        accumulated_ts = []
+        accumulated_normalized_ts = []
         sum = 0
         for year in xrange(2000, 2010):
             count = len(t['citing_paper_time_series'][year])
             sum += count
+            accumulated_ts.append(sum)
             ts.append(count)
 
         for i in xrange(len(ts)):
             normalized_ts.append(ts[i] * 1.0 / sum)
+            accumulated_normalized_ts.append(normalized_ts[-1])
 
         id = t['_id']
         data = {}
         data['_id'] = id
         data['ts'] = ts
         data['normalized_ts'] = normalized_ts
+        data['accumulated_ts'] = accumulated_ts
+        data['accumulated_normalized_ts'] = accumulated_normalized_ts
 
         all_data.append(data)
 
@@ -61,17 +67,24 @@ def plotAllTS(all_data, name):
 
     for data in all_data:
         sum = 0
+        count_sum = 0
+
+        for i in xrange(10):
+            count_sum += data['ts'][i]
+
         for i in xrange(5):
             sum += data['normalized_ts'][i]
 
-        if sum < 0.3 or sum > 0.7:
+        if count_sum > 500:# and (sum > 0.8 or sum < 0.2):
         # if random.randint(0, 200) > -1:
         #     continue
-            plt.plot(x, data[name])
+            plt.plot(x, data[name], label=str(data['_id']))
         # print data[name]
 
     plt.xticks(x, years)
-
+    plt.xlabel('Years')
+    plt.ylabel('Citation count' if name == 'ts' else 'Citation count proportion')
+    # plt.legend()
     # Save the figure in a separate file
     plt.savefig(name + '.png')
 
@@ -79,7 +92,7 @@ def plotAllTS(all_data, name):
     #plt.show()
 
 if __name__ == '__main__':
-    # preprocess()
+    preprocess()
     all_data = getAllData()
     plotAllTS(all_data, 'ts')
     plotAllTS(all_data, 'normalized_ts')
